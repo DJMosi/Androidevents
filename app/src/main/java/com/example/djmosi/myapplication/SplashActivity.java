a0479ca94ea0eb68fe53ed6846b255776ddca4ae
djmosi <davimosi>
2015-10-27 19:45:08 +0300

    init files

---
+++ app/src/main/java/com/example/djmosi/myapplication/SplashActivity.java
@@ -0,0 +1,39 @@
+package com.example.djmosi.myapplication;
+
+/**
+ * Created by djmosi on 10/27/15.
+ */
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+
+public class SplashActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        // TODO Auto-generated method stub
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.splash);
+
+        Thread timerThread = new Thread(){
+            public void run(){
+                try{
+                    sleep(3000);
+                }catch(InterruptedException e){
+                    e.printStackTrace();
+                }finally{
+                    Intent intent = new Intent(SplashScreen.this,MainActivity.class);
+                    startActivity(intent);
+                }
+            }
+        };
+        timerThread.start();
+    }
+
+    @Override
+    protected void onPause() {
+        // TODO Auto-generated method stub
+        super.onPause();
+        finish();
+    }
+}
